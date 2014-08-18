644bc6431f6d5b9a1b99e2ed2b384d5cbf9cbd59
Liza Carter <lizahcarter@gmail.com>
2014-08-18 17:06:04 -0500

    added index page

--- app/controllers/vacations.js
+++ app/controllers/vacations.js
@@ -1,9 +1,18 @@
 'use strict';
 
+var Vacation = require('../models/vacation'),
+    moment = require('moment');
+
 exports.create = function(req, res){
   console.log(req.body);
 };
 
 exports.new = function(req,res){
   res.render('vacations/new');
+};
+
+exports.index = function(req, res){
+  Vacation.all(function(err, vacations){
+  res.render('vacations/index', {vacations:vacations, moment:moment});
+  });
 };
