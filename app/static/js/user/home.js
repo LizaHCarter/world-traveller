1173684b49daaea864cedab5cf575128ffc72e9a
Liza Carter <lizahcarter@gmail.com>
2014-08-18 13:52:10 -0500

    added map api

--- app/static/js/user/home.js
+++ app/static/js/user/home.js
@@ -1,8 +1,15 @@
+/* global google:true */
 (function(){
   'use strict';
+  var map;
 
   $(document).ready(function(){
+    initMap(36, -87, 8);
   });
 
+ function initMap(lat, lng, zoom){
+    var mapOptions = {center: new google.maps.LatLng(lat, lng), zoom: zoom, mapTypeId: google.maps.MapTypeId.ROADMAP};
+    map = new google.maps.Map(document.getElementById('map'), mapOptions);
+  }
 })();
 
